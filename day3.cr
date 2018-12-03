0d9773147cae10a94c62180228ecaf8884ed2a37
Mark Janssen <mpc.janssen@gmail.com>
2018-12-03 19:41:36 +0100

    Prepare for 3-2

--- day3.cr
+++ day3.cr
@@ -1,19 +1,22 @@
 DIM = 1000 
-fabric = Array.new(DIM) { |i| Array.new(DIM) { |j| 0 } }
 
-claims = File.read_lines("input.3.txt")
-claims.each do | claim | 
-  orig, size =  claim.split("@")[1].split(":")
-  p orig.strip,size.strip
-  tx,ty = orig.strip.split(",")
-  tdx,tdy = size.strip.split("x")
-  x,y  = tx.to_i, ty.to_i
-  dx,dy  = tdx.to_i, tdy.to_i
-  (x...(x+dx)).each do | x |
-    (y...(y+dy)).each do | y |
-      fabric[x][y] += 1
+def build_claims
+  fabric = Array.new(DIM) { |i| Array.new(DIM) { |j| Array(String).new } }
+  claims = File.read_lines("input.3.txt")
+  claims.each do | claim | 
+    cid, coords = claim.split("@")
+    orig, size =  coords.split(":")
+    tx,ty = orig.strip.split(",")
+    tdx,tdy = size.strip.split("x")
+    x,y  = tx.to_i, ty.to_i
+    dx,dy  = tdx.to_i, tdy.to_i
+    (x...(x+dx)).each do | x |
+      (y...(y+dy)).each do | y |
+      fabric[x][y] << cid
+    end
     end
   end
+  fabric
 end
-p fabric
-p (fabric.map &.select { | x  | x > 1}.size).sum
+fabric = build_claims
+puts "Day3-1:", (fabric.map &.select { | x  | x.size > 1}.size).sum
